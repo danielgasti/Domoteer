4eda59c7346801c39bffd999a4265358ac58f5ed
f.ventura <francescoventura.183464@gmail.com>
2016-05-11 12:35:18 +0200

    DomoteerWebServer done; Implementing WebServices

--- Re_Do_Do/Program.cs
+++ Re_Do_Do/Program.cs
@@ -22,136 +22,36 @@ namespace Re_Do_Do
         public partial class Program
         {
 
-            GT.Networking.WebEvent sayHello;
 
-            byte[] HTML = Encoding.UTF8.GetBytes(
-                "<html><body>" +
-                "<h1>Hosted on .NET Gadgeteer</h1>" +
-                "<p>Lets scare someone!</p>" +
-                "<form action=\"\" method=\"post\">" +
-                "<input type=\"submit\" value=\"Toggle LED!\">" +
-                "</form>" +
-                "</body></html>");
+
+            DomoteerWebServer server;
 
             void ProgramStarted()
             {
-                //The other option is UseStaticIP
-                //ethernetJ11D.NetworkSettings.EnableDhcp();
-                //ethernetJ11D.NetworkSettings.EnableDynamicDns();
+
+                server = new DomoteerWebServer(ethernetJ11D,multicolorLED,displayT35);
+                server.initConnection();
+                server.RunWebServer();
+
                 
-                ethernetJ11D.UseThisNetworkInterface();
-                //ethernetJ11D.UseDHCP();
-
-                ethernetJ11D.NetworkSettings.EnableStaticIP("192.168.0.222", "255.255.255.0", "192.168.0.1");
-                //ethernetJ11D.NetworkSettings.PhysicalAddress = ByteExtensions.ToHexByte("002103804AF0");
-
-                //ethernetJ11D.UseStaticIP("192.168.1.222", "255.255.255.0", "192.168.1.254");
-
-                //Set a handler for when the network is available
-                ethernetJ11D.NetworkUp += ethernet_NetworkUp;
-                ethernetJ11D.NetworkDown += ethernet_NetwokDown;
-                multicolorLED.TurnBlue();
-                Debug.Print("Ip address: " + ethernetJ11D.NetworkSettings.IPAddress);
-                Debug.Print("MAC: " + ethernetJ11D.NetworkSettings.PhysicalAddress);
-                while (ethernetJ11D.NetworkSettings.IPAddress == "0.0.0.0")
-                {
-                    Debug.Print("Waiting for DHCP");
-                    Thread.Sleep(250);
-                }
-
-                new Thread(RunWebServer).Start();
-            }
-
-            void RunWebServer()
-            {
-                // Wait for the network...
-                while (ethernetJ11D.IsNetworkUp == false)
-                {
-                    Debug.Print("Waiting...");
-                    Thread.Sleep(1000);
-                }
-                // Start the server
-                WebServer.StartLocalServer(ethernetJ11D.NetworkSettings.IPAddress, 80);
-                WebServer.DefaultEvent.WebEventReceived += DefaultEvent_WebEventReceived;
-                while (true)
-                {
-                    Thread.Sleep(1000);
-                }
-            }
-
-            void DefaultEvent_WebEventReceived(string path, WebServer.HttpMethod method, Responder
-                responder)
-            {
-                // We always send the same page back
-                responder.Respond(HTML, "text/html;charset=utf-8");
-                // If a button was clicked
-                if (method == WebServer.HttpMethod.POST)
-                {
-                    multicolorLED.TurnWhite();
-                }
-            }
-
-            void ListNetworkInterfaces()
-            {
-                var settings = ethernetJ11D.NetworkSettings;
-
-                Debug.Print("------------------------------------------------");
-                Debug.Print("MAC: " + ByteExtensions.ToHexString(settings.PhysicalAddress, "-"));
-                Debug.Print("IP Address:   " + settings.IPAddress);
-                Debug.Print("DHCP Enabled: " + settings.IsDhcpEnabled);
-                Debug.Print("Subnet Mask:  " + settings.SubnetMask);
-                Debug.Print("Gateway:      " + settings.GatewayAddress);
-                Debug.Print("------------------------------------------------");
+                
             }
 
             
 
-            void ethernet_NetworkUp(Gadgeteer.Modules.Module.NetworkModule sender, Gadgeteer.Modules.Module.NetworkModule.NetworkState state)
-            {
-                //Set the URI for the resource
+            
 
-                multicolorLED.TurnGreen();
-                ListNetworkInterfaces();
-                string ipAddress = ethernetJ11D.NetworkSettings.IPAddress;
-                Debug.Print("indirizzo IP acquisito: " + ipAddress);
-                //WebServer.StartLocalServer(ipAddress, 80);
-                //sayHello = Gadgeteer.Networking.WebServer.SetupWebEvent("test");
-                //sayHello.WebEventReceived += new WebEvent.ReceivedWebEventHandler(sayHello_WebEventReceived);
-            }
+           
 
-            void sayHello_WebEventReceived(string path, WebServer.HttpMethod method, Responder responder)
-            {
-                string content = "<html><body><h1>Vediamo se funziona</h1></body></html>";
-                byte[] bytes = new System.Text.UTF8Encoding().GetBytes(content);
-                //responder.Respond(bytes, "text/html");
-                responder.Respond("Hello world");
-            }
+            
 
-            void ethernet_NetwokDown(Gadgeteer.Modules.Module.NetworkModule sender, Gadgeteer.Modules.Module.NetworkModule.NetworkState state)
-            {
-                Debug.Print("IP " + ethernetJ11D.NetworkSettings.IPAddress);
-                multicolorLED.TurnRed();
-            }
+            
 
-            void request_ResponseReceived(HttpRequest sender, HttpResponse response)
-            {
-                if (response.StatusCode == "200")
-                {
-                    //This only works if the bitmap is the 
-                    //same size as the screen it's flushing to
-                    response.Picture.MakeBitmap().Flush();
-                }
-                else
-                {
-                    //Show a helpful error message
-                    displayT35.SimpleGraphics.DisplayText("Request failed with status code " + response.StatusCode
-                        , Resources.GetFont(Resources.FontResources.NinaB), Color.White, 0, 0);
+            
 
-                    displayT35.SimpleGraphics.DisplayText("Response text: " + response.Text
-                           , Resources.GetFont(Resources.FontResources.NinaB), Color.White, 0, 50);
+            
 
-                }
-            }
+            
         }
 
         //    void ethernetENC28_NetworkDown(GTM.Module.NetworkModule sender, GTM.Module.NetworkModule.NetworkState state)
--- Re_Do_Do/WebServer.cs
+++ Re_Do_Do/WebServer.cs
@@ -4,60 +4,151 @@ using Gadgeteer.Modules.GHIElectronics;
 using GT = Gadgeteer;
 using GTM = Gadgeteer.Modules;
 using Gadgeteer.Networking;
+using System.Threading;
+using Gadgeteer;
+using System.Text;
 namespace Re_Do_Do
 {
     class DomoteerWebServer
     {
         EthernetJ11D eth;
         MulticolorLED led;
+        DisplayT35 display;
+
+        byte[] HTML = Encoding.UTF8.GetBytes(
+                "<html><body>" +
+                "<h1>Hosted on .NET Gadgeteer</h1>" +
+                "<p>Lets scare someone!</p>" +
+                "<form action=\"\" method=\"post\">" +
+                "<input type=\"submit\" value=\"Toggle LED!\">" +
+                "</form>" +
+                "</body></html>");
+
         GT.Networking.WebEvent sayHello;
         string ipAddress;
         
-        public DomoteerWebServer(EthernetJ11D eth, MulticolorLED led) 
+        public DomoteerWebServer(EthernetJ11D eth, MulticolorLED led, DisplayT35 display) 
         {
             this.eth = eth;
             this.led = led;
+            this.display = display;
         }
 
-        public void initConnection(EthernetJ11D eth, MulticolorLED led)
+        public void initConnection()
         {
-            
+
+            //NEW
+            //The other option is UseStaticIP
+            //ethernetJ11D.NetworkSettings.EnableDhcp();
+            //ethernetJ11D.NetworkSettings.EnableDynamicDns();
+            //eth.UseDHCP();
+
+            //eth.NetworkSettings.EnableStaticIP("192.168.0.222", "255.255.255.0", "192.168.0.1");
+            //eth.NetworkSettings.PhysicalAddress = ByteExtensions.ToHexByte("002103804AF0");
+
+            //eth.UseStaticIP("192.168.1.222", "255.255.255.0", "192.168.1.254");
+
+            //Set a handler for when the network is available
+            eth.UseThisNetworkInterface();
             eth.NetworkSettings.EnableDhcp();
-            eth.UseDHCP();
-            Debug.Print("indirizzo IP acquisito: " + ipAddress);
-            eth.NetworkDown += new GTM.Module.NetworkModule.NetworkEventHandler(ethernet_NetworkDown);
-            eth.NetworkUp += new GTM.Module.NetworkModule.NetworkEventHandler(ethernet_NetworkUp);
+            //eth.UseDHCP();
+            eth.NetworkSettings.EnableStaticIP("192.168.0.222", "255.255.255.0", "192.168.0.1");
+            //ListNetworkInterfaces();
+
+            eth.NetworkUp += ethernet_NetworkUp;
+            eth.NetworkDown += ethernet_NetwokDown;
+            led.TurnBlue();
 
         }
 
-
-        //    eth.UseDHCP();
-        //    eth.NetworkUp += new GTM.Module.NetworkModule.NetworkEventHandler(ethernet_NetworkUp);
-        //}
-
-        void ethernet_NetworkUp(GTM.Module.NetworkModule sender, GTM.Module.NetworkModule.NetworkState state)
+        public void RunWebServer()
         {
-            //debug visuale
+            new Thread(_RunWebServer).Start();
+        }
+
+        private void _RunWebServer()
+        {
+            // Wait for the network...
+            while (eth.IsNetworkUp == false)
+            {
+                Debug.Print("Waiting...");
+                Thread.Sleep(1000);
+            }
+            // Start the server
+            WebServer.StartLocalServer(eth.NetworkSettings.IPAddress, 80);
+            WebServer.DefaultEvent.WebEventReceived += DefaultEvent_WebEventReceived;
+            
+            while (true)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+
+
+        void ListNetworkInterfaces()
+        {
+            var settings = eth.NetworkSettings;
+
+            Debug.Print("------------------------------------------------");
+            Debug.Print("MAC: " + ByteExtensions.ToHexString(settings.PhysicalAddress, "-"));
+            Debug.Print("IP Address:   " + settings.IPAddress);
+            Debug.Print("DHCP Enabled: " + settings.IsDhcpEnabled);
+            Debug.Print("Subnet Mask:  " + settings.SubnetMask);
+            Debug.Print("Gateway:      " + settings.GatewayAddress);
+            Debug.Print("------------------------------------------------");
+        }
+
+        void ethernet_NetworkUp(Gadgeteer.Modules.Module.NetworkModule sender, Gadgeteer.Modules.Module.NetworkModule.NetworkState state)
+        {
+            //Set the URI for the resource
+            Debug.Print("Network up.");
             led.TurnGreen();
-            string ipAddress = eth.NetworkSettings.IPAddress;
-            Debug.Print("indirizzo IP acquisito: " + ipAddress);
-            WebServer.StartLocalServer(ipAddress, 80);
-            sayHello = WebServer.SetupWebEvent("test");
-            sayHello.WebEventReceived += new WebEvent.ReceivedWebEventHandler(sayHello_WebEventReceived);
+            ListNetworkInterfaces();
+            
         }
 
-        void sayHello_WebEventReceived(string path, WebServer.HttpMethod method, Responder responder)
-        {
-            string content = "<html><body><h1>Vediamo se funziona</h1></body></html>";
-            byte[] bytes = new System.Text.UTF8Encoding().GetBytes(content);
-            responder.Respond(bytes, "text/html");
-        }
+        
 
-        void ethernet_NetworkDown(GTM.Module.NetworkModule sender, GTM.Module.NetworkModule.NetworkState state)
+        void ethernet_NetwokDown(Gadgeteer.Modules.Module.NetworkModule sender, Gadgeteer.Modules.Module.NetworkModule.NetworkState state)
         {
+            Debug.Print("Network down.");
             led.TurnRed();
         }
 
+        void request_ResponseReceived(HttpRequest sender, HttpResponse response)
+        {
+            if (response.StatusCode == "200")
+            {
+                //This only works if the bitmap is the 
+                //same size as the screen it's flushing to
+                response.Picture.MakeBitmap().Flush();
+            }
+            else
+            {
+                //Show a helpful error message
+                display.SimpleGraphics.DisplayText("Request failed with status code " + response.StatusCode
+                    , Resources.GetFont(Resources.FontResources.NinaB), Color.White, 0, 0);
+
+                display.SimpleGraphics.DisplayText("Response text: " + response.Text
+                       , Resources.GetFont(Resources.FontResources.NinaB), Color.White, 0, 50);
+
+            }
+        }
+
+        void DefaultEvent_WebEventReceived(string path, WebServer.HttpMethod method, Responder
+                responder)
+        {
+            // We always send the same page back
+            responder.Respond(HTML, "text/html;charset=utf-8");
+            // If a button was clicked
+            if (method == WebServer.HttpMethod.POST)
+            {
+                led.TurnWhite();
+               
+            }
+        }
+
+        
 
     }
 }
