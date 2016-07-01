5ea2b6a80a7703894894d08430572c138ce1e3a2
Ven7u <francescoventura.183464@gmail.com>
2016-07-01 15:56:19 +0200

    cross class

--- DomoteerService/Cross.cs
+++ DomoteerService/Cross.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Runtime.Serialization;
 
 namespace DomoteerService
 {
+    [DataContract]
     public class Cross
     {
+        [DataMember] 
+        public String timestamp { get; set; } 
     }
 }
